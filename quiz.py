fc329dd504369aeb06b2fa8d95c7b87434e9ff5a
Stevie Migan <stevie@radcub.net>
2022-01-12 11:20:45 +0000

    Add dictionary and lists for questions and answers

--- quiz.py
+++ quiz.py
@@ -1,3 +1,37 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def start_game():
+    pass
+
+def check_answer():
+    pass
+
+def score_display():
+    pass
+
+def start_over():
+    pass
+
+questions = {
+    "What is the best selling console of all time?: ": "A",
+    "Who are the creators of the well-known Final Fantasy series?: ": "C",
+    "Who was the MAIN protagonist in Metal Gear Solid 2: Sons of Liberty?: ": "B",
+    "What is the maximum score in the classic arcade game, 'Pac-man'?: ": "D",
+    "What species of animal is Tom Nook?: ": "A",
+    "What color is the 1-up mushroom in the Super Mario series?: ": "B",
+    "What game popularized a new genre of game in 2011?: ": "C",
+    "Which games company is the biggest host for simulated tycoon games?: ": "A",
+    "Which one of these MMO's does Illidan Stormrage belong to?: ": "D",
+    "Which mobile game blew up the gaming world in 2016?: ": "C"
+}
+
+answers = [
+    ["A. Playstation 2", "B. Nintendo Switch", "C. Nintendo Wii", "D. Super Nintendo (SNES)"]
+    ["A. Electronic Arts (EA)", "B. FROM Software", "C. Square Enix", "D. Blizzard"]
+    ["A. Liquid Snake", "B. Raiden", "C. Big Boss", "D. Solidus"]
+    ["A. 999,999,999", "B. 255", "C. 65,535", "D. 3,335,360"]
+    ["A. Tanuki", "B. Raccoon", "C. Fox", "D. Badger"]
+    ["A. Red", "B. Green", "C. White", "D. Blue"]
+    ["A. The Elder Scrolls: Skyrim", "B. Dead Space 2", "C. Dark Souls", "D. Cars 2"]
+    ["A. Frontier", "B. Introversion Software", "C. 2 point studios", "D. Paradox interactive"]
+    ["A. Final Fantasy XIV", "B. Guild Wars", "C. Elder scrolls online", "D. World of Warcraft"]
+    ["A. Candy Crush", "B. Angry Birds", "C. Pokemon GO", "D. Temple Run"]
+]

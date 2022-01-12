b11eef3185f6bf17d876b8e08fadcea64564016f
Stevie Migan <stevie@radcub.net>
2022-01-12 16:13:26 +0000

    Add welcome and instructions text

--- run.py
+++ run.py
@@ -7,13 +7,12 @@ def start_game():
     for x in questions:
         print("********************")
         print(x)
+        print()
         for i in answers[question_number - 1]:
             print(i)
         guess = input("Please enter your answer! Is it (A, B, C or D?): ")
         guess = guess.upper()
         guesses.append(guess)
-        
-
         correct_answers += check_answer(questions.get(x), guess)
         question_number += 1
 
@@ -23,9 +22,11 @@ def start_game():
 def check_answer(answer, guess):
 
     if answer == guess:
+        print()
         print("Well done! That's the correct answer :)")
         return 1
     else:
+        print()
         print("Sorry, that's incorrect!")
         return 0
 
@@ -63,12 +64,12 @@ def start_over():
 questions = {
     "What is the best selling console of all time?: ": "A",
     "Who are the creators of the well-known Final Fantasy series?: ": "C",
-    "Who was the MAIN protagonist in Metal Gear Solid 2: Sons of Liberty?: ": "B",
+    "Who was the MAIN protagonist in Metal Gear Solid 2: ": "B",
     "What is the maximum score in the classic arcade game, 'Pac-man'?: ": "D",
     "What species of animal is Tom Nook?: ": "A",
     "What color is the 1-up mushroom in the Super Mario series?: ": "B",
     "What game popularized a new genre of game in 2011?: ": "C",
-    "Which games company is the biggest host for simulated tycoon games?: ": "A",
+    "Which company is the biggest host for simulated tycoon games?: ": "A",
     "Which one of these MMO's does Illidan Stormrage belong to?: ": "D",
     "Which mobile game blew up the gaming world in 2016?: ": "C"
 }
@@ -82,13 +83,23 @@ answers = [
     ["A. 999,999,999", "B. 255", "C. 65,535", "D. 3,335,360"],
     ["A. Tanuki", "B. Raccoon", "C. Fox", "D. Badger"],
     ["A. Red", "B. Green", "C. White", "D. Blue"],
-    ["A. The Elder Scrolls: Skyrim", "B. Dead Space 2", "C. Dark Souls", "D. Cars 2"],
+    ["A. The Elder Scrolls: Skyrim", "B. Dead Space 2",
+     "C. Dark Souls", "D. Cars 2"],
     ["A. Frontier", "B. Introversion Software",
         "C. 2 point studios", "D. Paradox interactive"],
     ["A. Final Fantasy XIV", "B. Guild Wars",
         "C. Elder scrolls online", "D. World of Warcraft"],
     ["A. Candy Crush", "B. Angry Birds", "C. Pokemon GO", "D. Temple Run"],
 ]
+
+print()
+print("Hello and welcome to...THE GAMING QUIZ!")
+print()
+print("This is a simple trivia game built in Python 3")
+print()
+print("You will be asked 10 Gaming questions and be scored out of 100%")
+print()
+print("Good Luck! :)")
 
 start_game()
 
